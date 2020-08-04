2190e202087b5a152a23ebd7ac5a89c11363b55b
YasaswiniU <yasaswini.ummadi@gmail.com>
2020-08-04 14:51:53 +0530

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,94 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'directives';
+  currentPage = 0;
+  images = [
+    {
+      title : 'At the beach',
+      url : 'https://images.unsplash.com/photo-1596362878972-9a15e138e730?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80'
+    },
+    {
+      title : 'At the forest',
+      url : 'https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
+    },
+    {
+      title : 'At the temple',
+      url : 'https://images.unsplash.com/photo-1554554497-0095c34db3ec?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=882&q=80'
+    },
+    {
+      title : 'At the City',
+      url : 'https://images.unsplash.com/photo-1519501025264-65ba15a82390?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80'
+    },
+    {
+      title : 'At the beach',
+      url : 'https://images.unsplash.com/photo-1596362878972-9a15e138e730?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80'
+    },
+    {
+      title : 'At the forest',
+      url : 'https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
+    },
+    {
+      title : 'At the temple',
+      url : 'https://images.unsplash.com/photo-1554554497-0095c34db3ec?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=882&q=80'
+    },
+    {
+      title : 'At the City',
+      url : 'https://images.unsplash.com/photo-1519501025264-65ba15a82390?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80'
+    },
+    {
+      title : 'At the beach',
+      url : 'https://images.unsplash.com/photo-1596362878972-9a15e138e730?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80'
+    },
+    {
+      title : 'At the forest',
+      url : 'https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
+    },
+    {
+      title : 'At the temple',
+      url : 'https://images.unsplash.com/photo-1554554497-0095c34db3ec?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=882&q=80'
+    },
+    {
+      title : 'At the City',
+      url : 'https://images.unsplash.com/photo-1519501025264-65ba15a82390?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80'
+    },
+    {
+      title : 'At the beach',
+      url : 'https://images.unsplash.com/photo-1596362878972-9a15e138e730?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80'
+    },
+    {
+      title : 'At the forest',
+      url : 'https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
+    },
+    {
+      title : 'At the temple',
+      url : 'https://images.unsplash.com/photo-1554554497-0095c34db3ec?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=882&q=80'
+    },
+    {
+      title : 'At the City',
+      url : 'https://images.unsplash.com/photo-1519501025264-65ba15a82390?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80'
+    },
+    {
+      title : 'At the beach',
+      url : 'https://images.unsplash.com/photo-1596362878972-9a15e138e730?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80'
+    },
+    {
+      title : 'At the forest',
+      url : 'https://images.unsplash.com/photo-1448375240586-882707db888b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80'
+    },
+    {
+      title : 'At the temple',
+      url : 'https://images.unsplash.com/photo-1554554497-0095c34db3ec?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=882&q=80'
+    },
+    {
+      title : 'At the City',
+      url : 'https://images.unsplash.com/photo-1519501025264-65ba15a82390?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80'
+    }
+  ];
+
+
+
+  checkWindowIndex(value: number){
+    return Math.abs(this.currentPage - value) < 5;
+  }
+
 }
